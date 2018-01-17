175fedd47a521b20f19d50279556fb1260bade33
Neal McCarthy <nmccarth@csis.svsu.edu>
2018-01-17 11:14:59 -0500

    Created a quadratic equation solver with php

---
+++ quadratic/quadratic.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Quadraticc Result</title>
+	</head>
+	<body>
+		<?php
+			$aVar = $_POST["aVar"];
+			$bVar = $_POST["bVar"];
+			$cVar = $_POST["cVar"];
+			$resOne = (-$bVar - sqrt(pow($b,2) - (4 * $aVar * $cVar))) / (2 * $aVar);
+			$resTwo = (-$bVar - sqrt(pow($b,2) + (4 * $aVar * $cVar))) / (2 * $aVar);
+			if(is_nan($resOne) && !is_nan($resTwo))
+				echo "Result is $resTwo";
+			else if(is_nan($resTwo) || !is_nan($resOne))
+				echo "Result is $resOne";
+			else if(!is_nan($resOne) && !is_nan($resTwo))
+				echo "Result is $resOne, $resTwo";
+			else
+				echo "Can't find result";
+
+
+			//echo "Result for ${aVar}x^2 + ${bVar}x + $cVar = 0: $resOne, $resTwo";
+		?>
+	</body>
+</html>
+
