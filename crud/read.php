9b1a762dcd7bf57135f3f16faa673c9cb4adb22b
Neal McCarthy <nmccarth@csis.svsu.edu>
2018-01-29 11:57:13 -0500

    Created a simple database app in class using php

---
+++ crud/read.php
@@ -0,0 +1,67 @@
+<?php
+require 'database.php';
+$id = null;
+if ( !empty($_GET['id'])) {
+	$id = $_REQUEST['id'];
+}
+
+if ( null==$id ) {
+	header("Location: index.php");
+} else {
+	$pdo = Database::connect();
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "SELECT * FROM customers where id = ?";
+	$q = $pdo->prepare($sql);
+	$q->execute(array($id));
+	$data = $q->fetch(PDO::FETCH_ASSOC);
+	Database::disconnect();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <div class="span10 offset1">
+	<div class="row">
+	     <h3>Read a Customer</h3>
+	</div>
+	<div class="">
+	    <div class="control-group">
+		<label class="form-control-label">Name</label>
+		<div class="controls">
+		    <label class="form-check">
+			<?php echo $data[ 'name'];?>
+		    </label>
+		</div>
+	    </div>
+	    <div class="control-group">
+		<label class="form-control-label">Email Address</label>
+		<div class="controls">
+		    <label class="form-check">
+			<?php echo $data[ 'email'];?>
+		    </label>
+		</div>
+	    </div>
+	    <div class="control-group">
+		<label class="form-control-label">Mobile Number</label>
+		<div class="controls">
+		    <label class="form-check">
+			<?php echo $data[ 'mobile'];?>
+		    </label>
+		</div>
+	    </div>
+	    <div class="form-actions"> <a class="btn" href="index.php">Back</a>
+	    </div>
+	</div>      
+
+
+</body>
+</html>
