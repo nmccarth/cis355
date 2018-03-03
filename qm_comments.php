9eb0fac3cbf680050c51943d02e75a64fd9e95d3
Neal McCarthy <nmccarth [at] svsu [dot] edu>
2018-03-03 16:04:08 -0500

    All operations work except update whihc adds a new entry instead of updating

--- qm_comments.php
+++ qm_comments.php
@@ -40,8 +40,8 @@ class QmComments {
 
 			// actions
 			echo '<td>';
-			echo '<a class="btn btn-secondary" href="qm_comments.php?oper=2&per='. $_GET['per'] .
-				'&ques=' . $_GET['ques'] . '">Read</a>';
+			echo '<a class="btn btn-secondary" href="qm_comments.php?oper=2&per='.  $_GET['per'] .
+				'&ques=' . $_GET['ques'] . '&com=' . $row['id'] . '">Read</a>';
 
 			echo ' ';
 			echo '<a class="btn btn-success" href="qm_comments.php?oper=3&per='. $_GET['per'] .
@@ -49,7 +49,7 @@ class QmComments {
 
 			echo ' ';
 			echo '<a class="btn btn-danger" href="qm_comments.php?oper=4&per='. $_GET['per'] .
-				'&ques=' . $_GET['ques'] . '&com=' . $row['id'] . '">Update</a>';
+				'&ques=' . $_GET['ques'] . '&com=' . $row['id'] . '">Delete</a>';
 
 			echo '</tr>';
 		}
@@ -58,57 +58,144 @@ class QmComments {
 	}
 
 	function createRow() {
-		// begin body section
-		echo '<body><div class="container">';
-
-		// title of page
-		echo '<div class="row"><h3>Create Comment</h3></div>';
-
-		echo '<form class="form-horizontal" action="qm__comments.php" method="get">';
-		echo '<div class="form-group row"><label for="comment" class="col-2 col-form-label">';
-		echo 'Comment</label><div class="col-10"><input class="form-control" type="text" id="comment';
-		echo '"></div></div><div class="form-group row"><label for="rating" class="col-2';
-		echo ' col-form-label">Rating</label><div class="col-10"><input class="form-control"';
-		echo ' type="number" id="rating"></div></div>';
-
-		echo '<div class="form-actions"><button type="submit" class="btn btn-success">Create</button';
-		echo '><a class="btn btn-danger" href="qm_per_list.php">Back</a></div></form>'
+		if ( !empty($_POST)) { // if not first time through
+			echo '<script>console.log("in posted");</script>';
+			$per = $_POST['per'];
+			$ques = $_POST['ques'];
+			$comment = $_POST['comment'];
+			$rating = $_POST['rating'];
 
 
-		$pdo = Database::connect();
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO qm_comments (per_id, ques_id, comment, rating) values(?, ?, ?, ?)";
-		$q = $pdo->prepare($sql);
-		$q->execute(array($GET_['per'], $GET_['ques'], $GET_['comment'], $GET_['rating']));
-		Database::disconnect();
-		header("Location: qm_comments");
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO qm_comments (per_id, ques_id, comment, rating) values(?, ?, ?, ?)"; 
+			$q = $pdo->prepare($sql);
+			$q->execute(array($per, $ques, $comment, $rating));
+			Database::disconnect();
 
+			header('Location: qm_comments.php?oper=0&ques=' . $ques . '&per=' . $per);
+		}
+		echo '<body> <div class="container"> <div class="span10 offset1">';
+		echo '<div class="row"> <h3>New Comment</h3> </div><form class="form-horizontal" action="qm_comments.php?oper=1" method="post">';
+
+		echo '<input type="hidden" name="per" value="' . $_GET['per'] . '">';
+		echo '<input type="hidden" name="ques" value="' . $_GET['ques'] . '">';
+		echo '<div class="form-group"><label for="comment">Comment: </label><input class="form-control" name="comment" id="comment"></div>';
+
+		echo '<div class="form-group"><label for="rating">Rating: </label><input type="numeric" class="form-control" name="rating" id="rating"></div>';
+
+		echo '<button type="submit" class="btn btn-success">Yes</button><span>   </span>';
+		echo '<a class="btn btn-danger" href="qm_comments.php?oper=0&per='. $_GET['per'] . '&ques=' . $_GET['ques'] . '">No</a>';
+		echo '</form></div>';
 	}
 
 	function readRow() {
-		// begin body section
-		echo '<body><div class="container">';
+		$id = $_GET['com'];
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT * FROM qm_comments where id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		$data = $q->fetch(PDO::FETCH_ASSOC);
+		Database::disconnect();
 
-		// title of page
-		echo '<div class="row"><h3>Read Comment</h3></div>';
+		echo '<body> <div class="container"> <div class="span10 offset1">';
+		echo '<div class="row"> <h3>Comment Details</h3> </div><div class="form-horizontal" >';
 
+		echo '<div class="form-group"><label for="id">ID: </label><input class="form-control" readonly value="' . $data['id'] . '"></div>';
+
+		echo '<div class="form-group"><label for="per_id">Person ID:</label><input class="form-control" readonly value="' . $data['per_id'] . '"></div>';
+
+		echo '<div class="form-group"><label for="ques_id">Question ID:</label><input class="form-control" readonly value="' . $data['ques_id'] . '"></div>';
+
+		echo '<div class="form-group"><label for="comment">Comment:</label><input class="form-control" readonly value="' . $data['comment'] . '"></div>';
+
+		echo '<div class="form-group"><label for="rating">Rating:</label><input class="form-control" readonly value="' . $data['rating'] . '"></div>';
+
+		echo '<a class="btn btn-primary" href="qm_comments.php?oper=0&per=' . $data['per_id'] . '&ques=' . $data['ques_id'] . '">Go Back</a>';
 	}
 
 	function updateRow() {
+		if ( !empty($_POST)) { // if $_POST filled then process the form
+			$id = $_POST['com'];
+			$comment = $_POST['comment'];
+			$rating = $_POST['rating'];
+			$per = $_POST['per'];
+			$ques = $_POST['ques'];
+
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "UPDATE qm_comments SET per_id = ? , ques_id = ?, comment = ?, rating = ? WHERE id= ?";
+// 			$sql = "INSERT INTO qm_comments (per_id, ques_id, comment, rating) values(?, ?, ?, ?) ON DUPLICATE KEY UPDATE id=".$id; 
+			$q = $pdo->prepare($sql);
+			$q->execute(array($per, $ques, $comment, $rating, $id));
+// 			$q->execute(array($per, $ques, $comment, $rating));
+			Database::disconnect();
+			header("Location: qm_comments.php?oper=0&ques=". $ques . '&per=' . $per);
+		} else {
+			$id = $_GET['com'];
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "SELECT * FROM qm_comments where id = ?";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($id));
+			$data = $q->fetch(PDO::FETCH_ASSOC);
+			Database::disconnect();
+
+		}
 		// begin body section
 		echo '<body><div class="container">';
 
 		// title of page
 		echo '<div class="row"><h3>Update Comment</h3></div>';
+		echo '<form class="form-horizontal" action="qm_comments.php?oper=1" method="post">';
+
+		echo '<input type="hidden" name="per" value="' . $_GET['per'] . '">';
+		echo '<input type="hidden" name="com" value="' . $_GET['com'] . '">';
+		echo '<input type="hidden" name="ques" value="' . $_GET['ques'] . '">';
+		echo '<div class="form-group"><label for="comment">Comment: </label><input class="form-control" name="comment" id="comment"'.
+			'value="' .$data['comment'] . '"></div>';
+
+		echo '<div class="form-group"><label for="rating">Rating: </label><input type="numeric" class="form-control" name="rating" id="rating"'.
+			'value="' . $data['rating'] . '"></div>';
+
+		echo '<button type="submit" class="btn btn-success">Yes</button><span>   </span>';
+		echo '<a class="btn btn-danger" href="qm_comments.php?oper=0&per='. $_GET['per'] . '&ques=' . $_GET['ques'] . '">No</a>';
+		echo '</form></div>';
 
 	}
 
 	function deleteRow() {
+		if ( !empty($_POST)) { // if user clicks "yes" (sure to delete), delete record
+			$id = $_POST['id'];
+			$ques = $_POST['ques'];
+			$per = $_POST['per'];
+
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "DELETE FROM qm_comments  WHERE id = ?";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($id));
+			Database::disconnect();
+			header("Location: qm_comments.php?oper=0&ques=". $ques . '&per=' . $per);
+
+		} 
 		// begin body section
 		echo '<body><div class="container">';
 
 		// title of page
-		echo '<div class="row"><h3>Comments</h3></div>';
+		echo '<div class="row"><h3>Delete Comment</h3></div>';
+
+		echo '<form class="form-horizontal" action="qm_comments.php?oper=4" method="post">';
+		echo '<input type="hidden" name="id" value="' . $_GET['com'] . '">';
+		echo '<input type="hidden" name="ques" value="' . $_GET['ques'] . '">';
+		echo '<input type="hidden" name="per" value="' . $_GET['per'] . '">';
+		echo '<p class="alert alert-error">Are you sure you want to delete ?</p>';
+		echo '<div class="form-actions">';
+		echo '<button type="submit" class="btn btn-danger">Yes</button><span>   </span>';
+		echo '<a class="btn btn-success" href="qm_comments.php?oper=0&per='. $_GET['per'] . '&ques=' . $_GET['ques'] . '">No</a>';
+		echo '</div></form><br></div>';
+
 
 	}
 }
