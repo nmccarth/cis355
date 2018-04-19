6cd64eb877c6a76d1bd6dbd715dd88a346805b81
nmccarth <nmccarth [at] svsu [dot] edu>
2018-04-19 16:41:49 -0400

    Fixed navbar

--- project/pj_person.php
+++ project/pj_person.php
@@ -10,17 +10,23 @@ include '../../database/database.php';
 include 'session.php';
 echo '
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Neal McCarthy</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link btn btn-warning" href="logout.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-  </div>
+	<a class="navbar-brand" href="#">Neal McCarthy</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">People <span class="sr-only">(current)</span></a>
+			</li>
+		</ul>
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="pj_projects.php">Projects</a>
+			</li>
+		</ul>
+	</div>
+	<a class="nav-link btn btn-warning" href="logout.php">Logout</a>
 </nav>
 ';
 interface ICommentsCrud{
